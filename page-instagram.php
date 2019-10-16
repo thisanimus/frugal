ccc8aadb36ed0f2f2515239511001473404e91f2
Andrew Hale <hale.adh@gmail.com>
2019-10-15 22:33:13 -0500

    Add support for wp photo widths and space before comments

--- page-instagram.php
+++ page-instagram.php
@@ -8,7 +8,15 @@
  * @since    Timber 0.1
  */
 
-$context = Timber::context();
+/**
+ * Template Name: Instagram
+ * Description: Template for the instagram page.
+ *
+ * @package  WordPress
+ * @subpackage  Timber
+ * @since    Timber 0.1
+ */
+ $context = Timber::context();
 
 $timber_post = new Timber\Post();
 $context['post'] = $timber_post;
